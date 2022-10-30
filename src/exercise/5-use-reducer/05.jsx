3af9612a1e5a50257ee56604166b0b6c374e526a
villou <dkulic01@gmail.com>
2022-10-30 10:06:38 +0100

    exercise instructions

--- src/exercise/5-use-reducer/05.jsx
+++ src/exercise/5-use-reducer/05.jsx
@@ -1,57 +1,18 @@
 import { useReducer } from 'react';
 
-const REDUCER_ACTIONS = {
-  INCREMENT: 'increment',
-  DECREMENT: 'decrement',
-  RESET: 'reset',
-};
-
-const reducer = (state, { action, value = 1 }) => {
-  if (!Number(value)) return state;
-  if (value > 10) return state;
-
-  switch (action) {
-    case REDUCER_ACTIONS.INCREMENT:
-      return state + value;
-    case REDUCER_ACTIONS.DECREMENT:
-      return state - value;
-    case REDUCER_ACTIONS.RESET:
-      return 0;
-    default:
-      throw new Error('Unexpected action');
-  }
+const reducer = (value, action) => {
+  // 🦁 Ici il faut que tu return la valeur incrémenté de 1
 };
 
 const Counter = () => {
-  const [count, dispatch] = useReducer(reducer, 0);
+  // 🦁 Remplace ceci par un useReducer avec `reducer` en paramètre et `0` en initialState
+  let count = 0;
 
   return (
     <div>
-      <button
-        onClick={() =>
-          dispatch({ action: REDUCER_ACTIONS.DECREMENT, value: 5000 })
-        }
-      >
-        -5
-      </button>
-      <button onClick={() => dispatch({ action: REDUCER_ACTIONS.DECREMENT })}>
-        -
-      </button>
       <button>{count}</button>
-      <button onClick={() => dispatch({ action: REDUCER_ACTIONS.INCREMENT })}>
-        +
-      </button>
-      <button
-        onClick={() =>
-          dispatch({ action: REDUCER_ACTIONS.INCREMENT, value: 5 })
-        }
-      >
-        +5
-      </button>
-      <br />
-      <button onClick={() => dispatch({ action: REDUCER_ACTIONS.RESET })}>
-        Reset
-      </button>
+      {/* 🦁 Ajoute un `onClick` qui appel la fonction dispatch de notre reducer */}
+      <button>+</button>
     </div>
   );
 };
